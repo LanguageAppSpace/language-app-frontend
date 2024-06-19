4ac12d72c163df51af4b59676d361486012b02f1
Weronika Jankowska <wjankowska8@gmail.com>
2024-06-19 12:47:23 +0200

    ✨ added LayoutPage

---
+++ src/pages/PageLayout.tsx
@@ -0,0 +1,25 @@
+import Navigation from '../components/Navigation/navigation'
+import Hero from '../components/Hero/hero'
+import Features from '../components/Features/features'
+import About from '../components/About/about'
+import Testimonials from '../components/Testimonials/testimonials'
+import Contact from '../components/Contact/contact'
+import Footer from '../components/Footer/footer'
+
+import './index.css'
+
+const PageLayout = () => {
+  return (
+    <> 
+      <Navigation />
+      <Hero />
+      <Features />
+      <About />
+      <Testimonials />
+      <Contact /> 
+      <Footer />
+    </>
+  )
+} 
+
+export default PageLayout
