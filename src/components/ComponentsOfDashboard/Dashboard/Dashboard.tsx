2dfd2290020038cb5004b0b3d8de71645c3c37f1
Weronika Jankowska <wjankowska8@gmail.com>
2024-06-26 17:59:16 +0200

    ⚡️ added useNavigate to Dashboard Sidebar

--- src/components/ComponentsOfDashboard/Dashboard/Dashboard.tsx
+++ src/components/ComponentsOfDashboard/Dashboard/Dashboard.tsx
@@ -8,6 +8,7 @@ import {
   ListItemText,
   Toolbar,
 } from "@mui/material";
+import { To, useNavigate } from "react-router-dom";
 
 import NavigationDashboard from "../NavigationOfDashboard/NavigationOfDashboard";
 import Main from "../Main/Main";
@@ -15,15 +16,25 @@ import Main from "../Main/Main";
 const drawerWidth = 240;
 
 const Sidebar = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = (path: To) => {
+    navigate(path);
+  };
+
   const drawer = (
     <div>
       <Toolbar />
       <List sx={{ backgroundColor: "rgb(5, 20, 50)", width: drawerWidth }}>
-        {["Dashboard", "New lesson", "Your lessons", "Logout"].map((text) => (
-          <ListItem key={text}>
-            <ListItemButton>
-              <ListItemText primary={text} />
-              <Box component="main"></Box>
+        {[
+          { text: "Dashboard", path: "/dashboard" },
+          { text: "New lesson", path: "/error" },
+          { text: "Your lessons", path: "/error" },
+          { text: "Logout", path: "/login" },
+        ].map((item) => (
+          <ListItem key={item.text} disablePadding>
+            <ListItemButton onClick={() => handleNavigation(item.path)}>
+              <ListItemText primary={item.text} sx={{ color: "white" }} />
             </ListItemButton>
           </ListItem>
         ))}
