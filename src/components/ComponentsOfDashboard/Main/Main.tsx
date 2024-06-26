0835bd2d867b5a975753dd4851f8b1ff494f3b4b
Weronika Jankowska <wjankowska8@gmail.com>
2024-06-26 17:50:29 +0200

    🎨 Added "review your lesson"

--- src/components/ComponentsOfDashboard/Main/Main.tsx
+++ src/components/ComponentsOfDashboard/Main/Main.tsx
@@ -1,7 +1,14 @@
-import { ROUTE } from '@/config/route.config';
-
-import { Box, Typography, Button, Grid, Card, CardContent, CircularProgress } from '@mui/material';
-import { To, useNavigate } from 'react-router-dom';
+import { ROUTE } from "@/config/route.config";
+import {
+  Box,
+  Typography,
+  Button,
+  Grid,
+  Card,
+  CardContent,
+  CircularProgress,
+} from "@mui/material";
+import { To, useNavigate } from "react-router-dom";
 
 const Main = () => {
   const navigate = useNavigate();
@@ -16,48 +23,63 @@ const Main = () => {
 
   const lessons = [
     {
-      title: 'Restaurant and Food',
-      phrases: '14 phrases',
+      title: "Restaurant and Food",
+      phrases: "14 phrases",
       progress: 50,
-      path: '/restaurant-and-food',
+      path: "/restaurant-and-food",
     },
     {
-      title: 'Traveling',
-      phrases: '25 phrases',
+      title: "Traveling",
+      phrases: "25 phrases",
       progress: 50,
-      path: '/traveling',
+      path: "/traveling",
     },
     {
-      title: 'Idioms about weather',
-      phrases: '15 phrases',
+      title: "Idioms about weather",
+      phrases: "15 phrases",
       progress: 50,
-      path: '/idioms-about-weather',
+      path: "/idioms-about-weather",
     },
   ];
 
   return (
-    <Box sx={{ width: '100%', p: 3 }}>
+    <Box sx={{ width: "100%", p: 3 }}>
       <Grid container spacing={3}>
         <Grid item xs={12} md={6}>
           <Box>
-          <Typography variant="h6" component="div" sx={{ color: "black", marginBottom: "15px"}}>
-              👋 Hello, <Typography variant="h5" component="span" fontWeight="bold">Anna!</Typography>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ color: "black", marginBottom: "15px" }}
+            >
+              👋 Hello,{" "}
+              <Typography variant="h5" component="span" fontWeight="bold">
+                Anna!
+              </Typography>
             </Typography>
-            <Typography variant="h6" component="span" sx={{ color: "black"}}>
-              Keep up the great work and don't let your <b>9</b>-day streak slip away!
+            <Typography variant="h6" component="span" sx={{ color: "black" }}>
+              Keep up the great work and don't let your <b>9</b>-day streak slip
+              away!
             </Typography>
           </Box>
         </Grid>
-        <Grid item xs={12} md={6} sx={{ textAlign: 'right' }}>
-          <img src="/people.jpg" alt="Hello" style={{ maxHeight: '300px' }} />
-          <Button 
-            size='large'
-            variant="contained" 
+        <Grid item xs={12} md={6} sx={{ textAlign: "right" }}>
+          <img src="/people.jpg" alt="Hello" style={{ maxHeight: "300px" }} />
+        </Grid>
+        <Grid item xs={12} md={6} sx={{ display: "flex", alignItems: "center", marginTop: "20px"}}>
+          <Typography variant="h6" component="span" sx={{ color: "black" }}>
+            Review your lessons
+          </Typography>
+        </Grid>
+        <Grid item xs={12} md={6} sx={{ textAlign: "right" }}>
+          <Button
+            size="large"
+            variant="contained"
             onClick={handleCreateNewLesson}
-            sx={{ 
+            sx={{
               mt: 2,
-              backgroundColor: "rgb(236, 177, 89)", 
-              borderRadius: "16px"
+              backgroundColor: "rgb(236, 177, 89)",
+              borderRadius: "16px",
             }}
           >
             Create new lesson
@@ -68,32 +90,32 @@ const Main = () => {
         </Grid>
         {lessons.map((lesson) => (
           <Grid item xs={12} md={4} key={lesson.title}>
-            <Card 
-              sx={{ 
+            <Card
+              sx={{
                 height: "150px",
-                backgroundColor: 'rgb(5, 20, 50)', 
-                color: 'white', 
-                cursor: 'pointer',
-                position: 'relative',
-              }} 
+                backgroundColor: "rgb(5, 20, 50)",
+                color: "white",
+                cursor: "pointer",
+                position: "relative",
+              }}
               onClick={() => handleLessonClick(lesson.path)}
             >
               <CardContent>
                 <Typography variant="h6">{lesson.title}</Typography>
-                <Typography variant="body2" sx={{ mt: 1, color: 'red' }}>
+                <Typography variant="body2" sx={{ mt: 1, color: "red" }}>
                   {lesson.phrases}
                 </Typography>
-                <Box 
+                <Box
                   sx={{
-                    position: 'absolute',
+                    position: "absolute",
                     top: 16,
                     right: 16,
                   }}
                 >
-                  <CircularProgress 
-                    variant="determinate" 
-                    value={lesson.progress} 
-                    sx={{ color: 'red' }} 
+                  <CircularProgress
+                    variant="determinate"
+                    value={lesson.progress}
+                    sx={{ color: "red" }}
                   />
                   <Box
                     sx={{
@@ -101,15 +123,15 @@ const Main = () => {
                       left: 0,
                       bottom: 0,
                       right: 0,
-                      position: 'absolute',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
+                      position: "absolute",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
                     }}
                   >
-                    <Typography 
-                      variant="caption" 
-                      component="div" 
+                    <Typography
+                      variant="caption"
+                      component="div"
                       color="white"
                     >{`${Math.round(lesson.progress)}%`}</Typography>
                   </Box>
