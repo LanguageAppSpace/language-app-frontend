4de44c15a551bdbe7cee53dcb5c2669269fec446
Ola Czyrnek <aleksandra@czyrnek.net>
2025-01-06 20:18:44 +0100

    Refactor Page404 styles (styled)

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,26 +1,29 @@
-import styled from "@emotion/styled";
+import { Box, Container, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 const Page404 = () => {
   return (
     <Page404Container>
-      <TextContainer>Error 404</TextContainer>
+      <Container maxWidth="xl">
+        <TextContainer>Error 404</TextContainer>
+      </Container>
     </Page404Container>
   );
 };
 
-const Page404Container = styled.div`
-  background-color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  width: 100vw;
-`;
+const Page404Container = styled(Box)({
+  backgroundColor: "white",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  position: "absolute",
+  inset: 0,
+});
 
-const TextContainer = styled.div`
-  color: black;
-  font-size: 3rem;
-  text-align: center;
-`;
+const TextContainer = styled(Typography)({
+  color: "black",
+  fontSize: "3rem",
+  textAlign: "center",
+});
 
 export default Page404;
