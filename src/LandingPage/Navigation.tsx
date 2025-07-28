a2d4220521349ca57062c2ccaea65f07b40f511c
Ola Czyrnek <aleksandra@czyrnek.net>
2025-07-28 22:20:22 +0200

    Add NaviPanel

--- src/LandingPage/Navigation.tsx
+++ src/LandingPage/Navigation.tsx
@@ -8,9 +8,16 @@ import { ROUTE } from "@/config/route.config.ts";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Logo from "@/components/Logo/Logo";
+import { useState } from "react";
+import NaviPanel from "@/components/NaviPanel/NaviPanel";
 
 const Navigation = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
   const navigate = useNavigate();
+
+  const toggleDrawer = (open: boolean) => () => {
+    setDrawerOpen(open);
+  };
 
   const handleLoginClick = () => {
     navigate(ROUTE.LOGIN);
@@ -25,6 +32,7 @@ const Navigation = () => {
             edge="start"
             color="inherit"
             aria-label="menu"
+            onClick={toggleDrawer(true)}
           >
             <MenuIcon />
           </StyledMenuIcon>
@@ -39,6 +47,7 @@ const Navigation = () => {
           </LogInButton>
         </Toolbar>
       </StyledAppBar>
+      <NaviPanel open={drawerOpen} onClose={toggleDrawer(false)} />
     </NavigationContainer>
   );
 };
