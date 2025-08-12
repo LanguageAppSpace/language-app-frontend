b74223b9e9d12ca73a03ffc33c8cbce7605d1570
Magdalena <magdalena.k9305@gmail.com>
2025-08-12 15:51:02 +0200

    add faqData to separate file

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -8,29 +8,7 @@ import {
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { styled } from "@mui/material/styles";
-
-const faqData = [
-  {
-    question: "Is the app free to use?",
-    answer:
-      "Yes! The app is free to use with optional premium features coming soon.",
-  },
-  {
-    question: "How can I reset my password?",
-    answer:
-      'You can reset your password by clicking "Forgot Password" on the login screen and following the instructions sent to your email.',
-  },
-  {
-    question: "How do I deactivate my account?",
-    answer:
-      "After logging in, go to the User Settings page where you will find an option to deactivate your account.",
-  },
-  {
-    question: "How can I change my personal data or password?",
-    answer:
-      "You can update your personal data or password in the User Settings section after logging into your account.",
-  },
-];
+import { faqData } from "@/constants/faqData";
 
 const Support = () => {
   return (
